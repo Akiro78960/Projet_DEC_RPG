89ad407b7f593521b9118b383fe4af0a79328526
Clovis <clo.perin@gmail.com>
2017-02-28 11:21:21 -0500

    Ajout de la librairie JSISO

--- Game/JS/main.js
+++ Game/JS/main.js
@@ -1,12 +1,13 @@
-var ctx = document.getElementById("canvas").getContext("2d")
+var ctx = null
 
 window.onload = function(){
-
+    ctx = document.getElementById("canvas").getContext("2d")
     tick()
 }
 
 
 function tick(){
     console.log("tick");
+    ctx.fillRect(0,0,20,20)
     window.requestAnimationFrame(tick)
 }
