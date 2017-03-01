1d152ea890c6c704f14c5ac783410a6184f04c3f
Clovis <clo.perin@gmail.com>
2017-03-01 12:42:54 -0500

    test JSISO: failed to install lol

--- Game/JS/main.js
+++ Game/JS/main.js
@@ -2,7 +2,12 @@ var ctx = null
 
 window.onload = function(){
     ctx = document.getElementById("canvas").getContext("2d")
-    tick()
+    require([
+        '/isometric/jsiso/canvas/Control',
+        '/isometric/jsiso/tile/Field',
+        '/isometric/requirejs/domReady!'
+    ])
+    fonction()
 }
 
 
@@ -11,3 +16,43 @@ function tick(){
     ctx.fillRect(0,0,20,20)
     window.requestAnimationFrame(tick)
 }
+
+fonction = function(CanvasControl, TileField) {
+
+     // RGBA of color to use
+     var tileColor = "(158, 154, 255, 1)";
+
+     // Our Tile Map
+     var tileMap = [
+       [tileColor, 0, tileColor, 0, tileColor],
+       [tileColor, tileColor, tileColor, 0, tileColor],
+       [tileColor, 0, tileColor, 0, tileColor]
+     ]
+
+     // X & Y drawing position, and tile span to draw
+     var xrange = 8;
+     var yrange = 8;
+
+     // use CanvasControl to create a simple canvas element
+     var context = CanvasControl.create("canvas", 600, 300, {}, "main");
+
+     var tileLayer = new TileField(context, CanvasControl().height, CanvasControl().width);
+
+     tileLayer.setup({
+       layout: tileMap,
+       isometric: false, // Flag used to layout grid in non isometric format
+       tileHeight: 80,
+       tileWidth: 80 // Try setting isometric to true and half tileWidth to 40, for an isometric map
+     });
+
+     // Rotate our entire Map
+     tileLayer.rotate("left");
+
+     // Loop through our tiles and draw the map
+     for (i = 0; i < xrange; i++) {
+       for (j = 0; j < yrange; j++) {
+         tileLayer.draw(i,j);
+       }
+     }
+
+   }
